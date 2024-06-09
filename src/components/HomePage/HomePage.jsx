59b9353ca0cf42ca40fb557a2da28f12aed1a31b
abrhamtafere <tafereab@gmail.com>
2024-06-10 02:26:05 +0300

    integration with the models api is working well

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -47,7 +47,7 @@ function HomePage() {
   const translateSentence = async (paragraph) => {
     try {
       setLoadingTranslate(true);
-      const response = await fetch("https://geeztoamharic.loca.lt/translate", {
+      const response = await fetch("http://127.0.0.1:5000/translate", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
