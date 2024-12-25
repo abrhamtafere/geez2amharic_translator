dd0b7c8f9c617a8b3f92fa95ee15fb4a0bcea446
abrhamtafere <88852639+abrhamtafere@users.noreply.github.com>
2024-12-25 08:36:57 +0300

    Update apiTags.js

--- src/constants/apiTags.js
+++ src/constants/apiTags.js
@@ -1,6 +1,6 @@
 // export const BASE_URL = "https://geeztoamharic.onrender.com/api/";
-export const BASE_URL = "https://geeztoamharic-8cx7.onrender.com/api/";
-export const BASE_URL2 = "http://localhost:5000/api/";
+// export const BASE_URL = "https://geeztoamharic-8cx7.onrender.com/api/";
+export const BASE_URL = "http://localhost:5000/api/";
 export const API_TAGS = {
   DATA: "Data",
   ADMIN: "ADMIN",
